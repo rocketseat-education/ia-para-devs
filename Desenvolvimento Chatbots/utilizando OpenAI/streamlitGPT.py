17a124644bbbf91fe6d8d9a8ff956e1ff65ec268
Vitor Hugo da Costa Luz <vitorhugoclz@gmail.com>
2024-11-20 18:04:22 -0300

    Desenvolvendo Chatbots - Solução Integrada (OpenAI + Watson): OpenAI vs Assistant

--- Desenvolvimento Chatbots/utilizando OpenAI/streamlitGPT.py
+++ Desenvolvimento Chatbots/utilizando OpenAI/streamlitGPT.py
@@ -17,7 +17,9 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
-    instructions = "Responda as perguntas do usuário de maneira informal"
+    instructions = """Você é um secretário de uma clinica médica. Você atendem várias especialidades médicas.
+    Interaja com o usuário e responda normalmente as perguntas dele, quando ele quiser marcar uma consulta pergunte na ordem. 1. Qual plano de saude ele possui
+    2. Melhor data e horário para consulta. Se o plano de saúde for o Notredame, informe ao usuário que não atendemos esse plano."""
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)

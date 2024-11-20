be75478c62feeb4054a5a4770e302191174f5b12
Vitor Hugo da Costa Luz <vitorhugoclz@gmail.com>
2024-11-20 17:39:11 -0300

    esenvolvendo Chatbots - Utilizando OpenAI: Introdução ao Streamlit

--- Desenvolvimento Chatbots/utilizando OpenAI/streamlitGPT.py
+++ Desenvolvimento Chatbots/utilizando OpenAI/streamlitGPT.py
@@ -5,3 +5,13 @@ st.title("ChatGPT Like")
 
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])

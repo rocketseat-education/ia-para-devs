48d0b5cce1e492796578976b26320b9294b50765
Vitor Hugo da Costa Luz <vitorhugoclz@gmail.com>
2024-11-20 17:47:04 -0300

    Desenvolvendo Chatbots - Utilizando OpenAI: Streamlit e OpenAI

--- Desenvolvimento Chatbots/utilizando OpenAI/streamlitGPT.py
+++ Desenvolvimento Chatbots/utilizando OpenAI/streamlitGPT.py
@@ -15,3 +15,21 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    instructions = "Responda as perguntas do usuário de maneira informal"
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": "user", "content": prompt},
+                {"role": "system", "content": instructions}
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})    
